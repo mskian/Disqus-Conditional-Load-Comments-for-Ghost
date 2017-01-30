82413aa18cee2ab0530c1c726b287c1e83374b35
Santhosh veer <mskian@users.noreply.github.com>
2017-01-30 19:37:39 +0530

    add file

---
+++ index.js
@@ -0,0 +1,9 @@
+<script type="text/javascript">
+ window.onscroll = function(e) {
+ if ((window.innerHeight + window.scrollY) 
+ >= document.body.offsetHeight) 
+ {
+ if (!disqus_loaded) disqus(); 
+ }
+};
+</script>
